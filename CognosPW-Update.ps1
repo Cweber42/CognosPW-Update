1631f007c9106e7c668e099ef229691f3b13e908
Cweber42 <xeniogame@gmail.com>
2017-06-05 09:52:48 -0500

    Add files via upload

---
+++ CognosPW-Update.ps1
@@ -0,0 +1,7 @@
+﻿#Script to create a password file for Cognos download Directory
+#This script MUST BE RAN LOCALLY to work properly! Run it on the same machine doing the cognos downloads, this does not work remotely!
+
+$savelocation = 'C:\scripts\apscnpw.txt' #Location to export the file
+
+
+Read-Host "Enter Password" -AsSecureString |  ConvertFrom-SecureString | Out-File $savelocation
